4a98d5dd3783f053893a4f969247fc6baafbd5b8
Developer Yasir <yasirminhas295@gmail.com>
2025-07-15 15:36:31 +0500

    new project start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <h1 className='bg-red-400'>hello</h1>
+  )
+}
+
+export default App
